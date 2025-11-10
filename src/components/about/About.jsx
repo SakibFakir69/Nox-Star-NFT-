d725d6e3a52e81aeaad9583fec200253bca54c0e
Sakib Fakir <FakirSakib22232@gmail.com>
2025-11-10 16:01:18 +0800

    about - about bug fix

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,61 +1,36 @@
+import React from 'react';
+import aboutCss from './../about/about.module.css';
 
-
-import React from 'react'
-import aboutCss from './../about/about.module.css'
 function About() {
   return (
     <div className={`${aboutCss.About_wrapper} section`}>
+      <h2 className="Section_Title  font-bold md:text-4xl text-xl text-emerald-400"><span className='text-white'>About</span> Us</h2>
 
-        <h2 className='Section_Title'><span>About</span> Us</h2>
+      <p className={aboutCss.Section_paragraph}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima temporibus ea, eum quas magnam reprehenderit sunt ex repellat, id debitis dolor quos repudiandae aliquid eaque. Officiis, possimus. Veniam, placeat obcaecati.
+      </p>
 
+      <div className={aboutCss.Counters}>
+        <p><span>2024+</span> Total Items</p>
+        <p><span>200+</span> WhiteList Profiles</p>
+      </div>
 
-        <p className={aboutCss.Section_paragraph}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima temporibus ea, eum quas magnam reprehenderit sunt ex repellat, id debitis dolor quos repudiandae aliquid eaque. Officiis, possimus. Veniam, placeat obcaecati.</p>
-
-        <div className={aboutCss.Counters}>
-            <p className='Counters'>  <span>2024+</span> Total Items </p>
-              <p className='Counters'>  <span>200+</span> WhiteList profiles </p>
-        </div>
-        <div className={aboutCss.AboutCard}>
-
-            <div className={`${aboutCss.About_card} card`} count='1'>
-                <span></span>
-                 <span></span>
-
-                <h2>High Quality</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et iste reprehenderit eveniet nisi, tenetur in earum, tempore deserunt aperiam doloribus, quaerat recusandae esse.</p>
-
-            </div>
-            <div className={`${aboutCss.About_card} card`} count='2'>
-                <span></span>
-                 <span></span>
-
-                <h2>Big Collection</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et iste reprehenderit eveniet nisi, tenetur in earum, tempore deserunt aperiam doloribus, quaerat recusandae esse.</p>
-
-            </div>
-            <div className={`${aboutCss.About_card} card`} count='3'>
-                <span></span>
-                 <span></span>
-
-                <h2>Top  Rescources</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et iste reprehenderit eveniet nisi, tenetur in earum, tempore deserunt aperiam doloribus, quaerat recusandae esse.</p>
-
-            </div>
-            <div className={`${aboutCss.About_card} card`} count='4'>
-                <span></span>
-                 <span></span>
-
-                <h2>Big Community</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et iste reprehenderit eveniet nisi, tenetur in earum, tempore deserunt aperiam doloribus, quaerat recusandae esse.</p>
-
-            </div>
-
-        </div>
-      
-
-
+      <div className={aboutCss.AboutCard}>
+        {[
+          { title: 'High Quality', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et iste reprehenderit eveniet nisi' },
+          { title: 'Big Collection', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et iste reprehenderit eveniet nisi' },
+          { title: 'Top Resources', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et iste reprehenderit eveniet nisi' },
+          { title: 'Big Community', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et iste reprehenderit eveniet nisi' }
+        ].map((item, i) => (
+          <div key={i} className={`${aboutCss.About_card} card`} data-count={i + 1}>
+            <span></span><span></span>
+            <h2>{item.title}</h2>
+            <p>{item.text}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
